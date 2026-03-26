e67950d38b46baca7d44cc58efea5d5f442a908b
Adnate IT Solutions <adnate@ankitadnate.onmicrosoft.com>
2026-03-26 17:26:23 +0530

    Added Affinda Resume parser

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apyhubApiKey: 'APY0tdwcgtk8I45k8jffmxBZioFjwzedLtidefoAzl1e9rWzjZem7xvVnOmNaBHm3PyCMOLB',
+  affindaApiKey: 'aff_565a220a59e9ef7f7621d195217171a6b2b85400',
 };
